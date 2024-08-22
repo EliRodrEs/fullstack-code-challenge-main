91803979074baede40e524cb4a299bcb83b3feae
EliRodrEs <elisabetre.main@gmail.com>
2024-08-22 18:23:49 +0200

    feat: add search-recipes base template

---
+++ recipes-book-frontend/src/components/SearchRecipes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="search-recipes">
+      <h2>Search Recipes</h2>
+      <form @submit.prevent="searchRecipes">
+        <div class="form-group">
+          <label for="name">Recipe name:</label>
+          <input type="text" id="name" v-model="name" />
+        </div>
+        <div class="form-group">
+          <label for="cookingTime">Cooking Time (minutes):</label>
+          <input type="number" id="cookingTime" v-model.number="cookingTime" />
+        </div>
+        <button type="submit">Search</button>
+      </form>
+      <div class="recipes-list" v-if="recipes.length > 0">
+        <div v-for="recipe in recipes" :key="recipe._id" class="recipe-card">
+          <h3>{{ recipe.name }}</h3>
+          <p><span class="text-bold">Difficulty:</span> {{ recipe.difficulty }}</p>
+          <p><span class="text-bold">Description:</span> {{ recipe.description }}</p>
+          <p><span class="text-bold">Cooking Time:</span> {{ recipe.cookingTime }} minutes</p>
+          <p><span class="text-bold">Ingredients:</span> {{ recipe.ingredients.join(', ') }}</p>
+        </div>
+      </div>
+      <div v-else-if="searched">No recipes found.</div>
+    </div>
+  </template>
--- recipes-book-frontend/src/views/HomeView.vue
+++ recipes-book-frontend/src/views/HomeView.vue
@@ -5,7 +5,10 @@
     <h1>Recipes Book</h1>
     <section class="action-sec">
       <RecipesBook />
-      <CreateRecipe />
+      <section class="interact">
+        <CreateRecipe />
+        <SearchRecipes />
+      </section>
     </section>
   </main>
 </template>
@@ -13,6 +16,7 @@
 <script setup lang="ts">
 import RecipesBook from '../components/RecipesBook.vue'
 import CreateRecipe from '../components/CreateRecipe.vue'
+import SearchRecipes from '../components/SearchRecipes.vue'
 </script>
 
 <style scoped>
@@ -20,5 +24,10 @@ import CreateRecipe from '../components/CreateRecipe.vue'
     display: grid;
     grid-template-columns: 1fr 1fr;
     column-gap: 2rem;
+  }
+
+  .interact {
+    display: grid;
+    row-gap: 2rem;
   }
 </style>
