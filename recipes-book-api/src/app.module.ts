b2b0803d6e5ee8550ef85684d123cd8a8e870df8
EliRodrEs <elisabetre.main@gmail.com>
2024-08-22 17:19:16 +0200

    feat: add dotenv dep

--- recipes-book-api/src/app.module.ts
+++ recipes-book-api/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipesModule } from './recipes/recipes.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 @Module({
   imports: [
     RecipesModule,
-    MongooseModule.forRoot(process.env.DATABASE_URL), //I know this is not advisable for complex projects. But time's short and we should always have env variables for things like this
+    MongooseModule.forRoot(process.env.DATABASE_URL)
   ],
 })
 export class AppModule {}
